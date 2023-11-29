81da882a9c89cd4448b6cc472a8d9fc2f340aa6c
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2023-11-29 14:27:08 -0300

    fix: updated timeout message

--- src/main/java/br/com/nicomaia/pingserver/PingHandler.java
+++ src/main/java/br/com/nicomaia/pingserver/PingHandler.java
@@ -22,16 +22,20 @@ public class PingHandler extends TextWebSocketHandler {
             if (address.isReachable(60000)) {
                 session.sendMessage(new TextMessage(sendDiff(address, started)));
             } else {
-                session.sendMessage(new TextMessage("Esgotado o tempo limite."));
+                session.sendMessage(new TextMessage(sendTimeout(address)));
             }
 
             Thread.sleep(1000);
         }
     }
 
-    public String sendDiff(InetAddress address, LocalDateTime started) {
+    private String sendDiff(InetAddress address, LocalDateTime started) {
         LocalDateTime finished = LocalDateTime.now();
 
         return String.format("%s: tempo=%dms%n", address.getHostAddress(), ChronoUnit.MILLIS.between(started, finished));
+    }
+
+    private String sendTimeout(InetAddress address) {
+        return String.format("%s: Timeout", address.getHostAddress());
     }
 }
